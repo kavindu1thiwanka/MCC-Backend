aeac87603a319c976f1af389159bd457eed0d568
Kavindu Thiwanka <101232856+kavindu1thiwanka@users.noreply.github.com>
2025-02-15 17:21:55 +0530

    remove comment

--- src/main/java/com/bms/repository/impl/VehicleManagementCustomRepositoryImpl.java
+++ src/main/java/com/bms/repository/impl/VehicleManagementCustomRepositoryImpl.java
@@ -25,7 +25,6 @@ public class VehicleManagementCustomRepositoryImpl implements VehicleManagementC
                 "FROM VehicleMst car WHERE car.status='A' AND car.availability='Y' AND car.category='" +  commonFilterDto.getCategory() + "'";
         sql = setConditions(sql, commonFilterDto);
         sql = setOrderBy(sql, commonFilterDto);
-        System.out.println(sql);
         Query query = entityManager.createQuery(sql);
         return query.getResultList();
     }
