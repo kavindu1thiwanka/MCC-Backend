aaecc11b55faa186ff28a0ee9f3636f8a0ff03ab
Kavindu Thiwanka <101232856+kavindu1thiwanka@users.noreply.github.com>
2025-02-16 16:40:24 +0530

    vehicle list retrieving api update

--- src/main/java/com/bms/dto/CommonFilterDto.java
+++ src/main/java/com/bms/dto/CommonFilterDto.java
@@ -3,6 +3,7 @@ package com.bms.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -12,4 +13,6 @@ public class CommonFilterDto {
     private String sortBy;
     private String category;
     private List<String> filters;
+    private Date pickUpDate;
+    private Date returnDate;
 }
--- src/main/java/com/bms/service/impl/VehicleManagementServiceImpl.java
+++ src/main/java/com/bms/service/impl/VehicleManagementServiceImpl.java
@@ -2,6 +2,8 @@ package com.bms.service.impl;
 
 import com.bms.dto.CommonFilterDto;
 import com.bms.dto.VehicleMstDto;
+import com.bms.entity.ReservationMst;
+import com.bms.repository.ReservationMstRepository;
 import com.bms.repository.VehicleManagementCustomRepository;
 import com.bms.service.VehicleManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +12,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 @Service
 @Transactional
 public class VehicleManagementServiceImpl implements VehicleManagementService {
 
     private VehicleManagementCustomRepository vehicleManagementCustomRepository;
+    private ReservationMstRepository reservationMstRepository;
 
     /**
      * This method is used to get vehicle list based on filter
@@ -24,7 +32,54 @@ public class VehicleManagementServiceImpl implements VehicleManagementService {
      */
     @Override
     public ResponseEntity<Object> getVehicleList(CommonFilterDto commonFilterDto) {
-        return new ResponseEntity<>(vehicleManagementCustomRepository.getVehicleList(commonFilterDto), HttpStatus.OK);
+
+        List<VehicleMstDto> vehicleList = vehicleManagementCustomRepository.getVehicleList(commonFilterDto);
+        HashMap<String, List<ReservationMst>> reservationMap = getReservationMap(commonFilterDto.getCategory());
+
+        List<VehicleMstDto> availableVehicleList = new ArrayList<>();
+
+        for (VehicleMstDto vehicle : vehicleList) {
+            if (!reservationMap.containsKey(vehicle.getVehicleNo())) {
+                availableVehicleList.add(vehicle);
+                continue;
+            }
+
+            boolean unavailable = false;
+            for (ReservationMst reservation : reservationMap.get(vehicle.getVehicleNo())) {
+
+                unavailable = (commonFilterDto.getPickUpDate().after(reservation.getPickUpDate())
+                        && commonFilterDto.getPickUpDate().before(reservation.getReturnDate()))
+                        || (commonFilterDto.getReturnDate().after(reservation.getPickUpDate())
+                        && commonFilterDto.getReturnDate().before(reservation.getReturnDate()));
+
+                if (unavailable) {
+                    break;
+                }
+            }
+            if (!unavailable) {
+                availableVehicleList.add(vehicle);
+            }
+        }
+        return new ResponseEntity<>(availableVehicleList, HttpStatus.OK);
+    }
+
+    private HashMap<String, List<ReservationMst>> getReservationMap(String category) {
+        List<Object[]> alreadyBookedVehicleList = reservationMstRepository.getAlreadyBookedVehicles(category);
+
+        HashMap<String, List<ReservationMst>> reservationMap = new HashMap<>();
+
+        for (Object[] reservation : alreadyBookedVehicleList) {
+            reservationMap.computeIfPresent((String) reservation[0], (k, v) -> {
+                v.add((ReservationMst) reservation[1]);
+                return v;
+            });
+            reservationMap.computeIfAbsent((String) reservation[0], k -> {
+                List<ReservationMst> reservationMstList = new ArrayList<>();
+                reservationMstList.add((ReservationMst) reservation[1]);
+                return reservationMstList;
+            });
+        }
+        return reservationMap;
     }
 
     @Override
@@ -35,5 +90,10 @@ public class VehicleManagementServiceImpl implements VehicleManagementService {
     @Autowired
     public void setVehicleManagementCustomRepository(VehicleManagementCustomRepository vehicleManagementCustomRepository) {
         this.vehicleManagementCustomRepository = vehicleManagementCustomRepository;
+    }
+
+    @Autowired
+    public void setReservationMstRepository(ReservationMstRepository reservationMstRepository) {
+        this.reservationMstRepository = reservationMstRepository;
     }
 }
